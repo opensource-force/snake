d584a13a3bdc55232651d20f8c6198d71b443bf7
wick3dr0se <wick3dr0se@protonmail.com>
2023-07-24 23:26:21 -0500

    rewrite w/ diff logic; more efficent

--- src/snake.nim
+++ src/snake.nim
@@ -1,112 +1,80 @@
-import os, terminal, strutils, random, term
+import terminal, term, os, strutils, random
 
 let (cols, rows) = terminalSize()
 
 var
+  dirX: int = 1
+  dirY: int
   snakeX: int = cols div 2
   snakeY: int = rows div 2
-  snakeBody: seq[(int, int)] = @[]
-  snakeLength: int = 3
-  direction: char = 'U'
+  snakeCells: seq[(int, int)] = @[]
+  snakeLen: int = 3
   foodX, foodY: int
-  delay: int = 700
 
 proc initTerm() =
   altBuffer()
   hideCursor()
-  setMode(noecho)
 
-proc gameOver(n: int) =
-  for _ in 0..<n:
+proc gameOver(i: int) =
+  for _ in 0..<i:
     setCursorPos(cols div 2 - 4, rows div 2)
-    echoRandColor("GAME OVER")
-    stdout.flushFile
+    echoRandColor("Game Over")
+    flushFile(stdout)
     sleep(150)
 
   mainBuffer()
   showCursor()
-  setMode(cooked)
   quit()
 
-proc drawSnake() =
+proc drawFood() =
+  foodX = rand(cols)
+  foodY = rand(rows)
+
+  setCursorPos(foodX, foodY)
+  stdout.styledWrite(bgRed, " ")
+
+initTerm()
+randomize()
+drawFood()
+
+while true:
+  case getch().toLowerAscii
+  of 'q': gameOver(3)
+  of 'h', 'a':
+    if dirX != 1:
+      dirX = -1; dirY = 0
+  of 'j', 's':
+    if dirY != -1:
+      dirX = 0; dirY = 1
+  of 'k', 'w':
+    if dirY != 1:
+      dirX = 0; dirY = -1
+  of 'l', 'd':
+    if dirX != -1:
+      dirX = 1; dirY = 0
+  else: continue
+
+  snakeX += dirX; snakeY += dirY
+
   setCursorPos(snakeX, snakeY)
   stdout.styledWrite(bgGreen, " ")
 
-proc drawFood() =
-  setCursorPos(foodX, foodY)
-  stdout.write(randColorSeq(bg), " \e[m")
+  snakeCells.add((snakeX, snakeY))
+  if len(snakeCells) > snakeLen:
+    for i in 0..<len(snakeCells) - snakeLen:
+      let (eraseX, eraseY) = snakeCells[i]
 
-proc moveSnake(x, y: int, snakeX, snakeY: var int) =
-  if x == -1:
-    if direction == 'R': snakeX += 1
-    else: direction = 'L'; snakeX -= 1
-  elif y == 1:
-    if direction == 'U': snakeY -= 1
-    else: direction = 'D'; snakeY += 1
-  elif y == -1:
-    if direction == 'D': snakeY += 1
-    else: direction = 'U'; snakeY -= 1
-  elif x == 1:
-    if direction == 'L': snakeX -= 1
-    else: direction = 'R'; snakeX += 1
-
-proc placeFood() =
-  while true:
-    foodX = rand(cols - 2) + 1
-    foodY = rand(rows - 2) + 1
-    if (foodX, foodY) notin snakeBody and foodX != snakeX and foodY != snakeY:
-      break
-  drawFood()
-
-proc consumeFood() =
-  if snakeX == foodX and snakeY == foodY:
-    snakeLength += 1
-    placeFood()
-    delay -= 25
-
-proc selfCollision() =
-  if (snakeX, snakeY) in snakeBody[0..<len(snakeBody)-1]:
-    gameOver(10)
-
-proc wallCollision() =
-  if snakeX == -1 or snakeX == cols or snakeY == -1 or snakeY == rows:
-    gameOver(10)
-
-proc keyMap(): bool =
-  case getch().toUpperAscii
-  of 'Q': gameOver(6)
-  of 'H', 'A': moveSnake(-1, 0, snakeX, snakeY)
-  of 'J', 'S': moveSnake(0, 1, snakeX, snakeY)
-  of 'K', 'W': moveSnake(0, -1, snakeX, snakeY)
-  of 'L', 'D': moveSnake(1, 0, snakeX, snakeY)
-  of '\e':
-    case getch() & getch()
-    of "[D": moveSnake(-1, 0, snakeX, snakeY)
-    of "[B": moveSnake(0, 1, snakeX, snakeY)
-    of "[A": moveSnake(0, -1, snakeX, snakeY)
-    of "[C": moveSnake(1, 0, snakeX, snakeY)
-  else: return false
-  return true
-
-randomize()
-initTerm()
-placeFood()
-
-while true:
-  if not keyMap(): continue
-
-  drawSnake()
-
-  selfCollision()
-  wallCollision()
-  consumeFood()
-
-  snakeBody.add((snakeX, snakeY))
-  if len(snakeBody) > snakeLength:
-    for i in 0..<len(snakeBody) - snakeLength:
-      let (eraseX, eraseY) = snakeBody[i]
       setCursorPos(eraseX, eraseY)
       stdout.styledWrite(bgDefault, " ")
-    snakeBody = snakeBody[len(snakeBody)-snakeLength..<len(snakeBody)]
 
-  sleep(delay)
+    snakeCells = snakeCells[len(snakeCells)-snakeLen..<len(snakeCells)]
+
+  if (snakeX, snakeY) in snakeCells[0..<len(snakeCells)-1]:
+    gameOver(4)
+
+  if snakeX == -1 or snakeX == cols or snakeY == -1 or snakeY == rows:
+    gameOver(4)
+
+  if (snakeX, snakeY) == (foodX, foodY):
+    drawFood()
+    snakeLen += 1
